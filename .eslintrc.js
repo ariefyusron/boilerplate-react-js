9cef76782d38402298415a43f0b771ca866348c8
Arief Yusron <ariefyusron98@gmail.com>
2020-02-24 18:43:50 +0700

    setup eslint && husky

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  "extends": "react-native-wcandillon",
+  "rules": {
+    "react/no-array-index-key": "off",
+    "react/jsx-no-bind": "off",
+    "import/prefer-default-export": "off",
+    "no-underscore-dangle": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "react-hooks/exhaustive-deps": "off",
+    "no-console": "error",
+    "no-undef": "off",
+    "no-unused-expressions": "off",
+    "@typescript-eslint/camelcase": "off",
+    "react/jsx-curly-newline": "off",
+    "array-callback-return": "off",
+    "jsx-a11y/no-static-element-interactions": "off",
+    "jsx-a11y/click-events-have-key-events": "off",
+    "@typescript-eslint/no-non-null-assertion": "off"
+  }
+};
